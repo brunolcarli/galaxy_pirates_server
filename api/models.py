95717f1d217b84d15e0577efb635f4f9d8e8b59c
brunolcarli <brunolcarli@gmail.com>
2025-04-30 02:11:31 -0300

    ADD universe

--- api/models.py
+++ api/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Ship(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False)
+    description = models.TextField()
+    offense_power = models.IntegerField()
+    shield_power = models.IntegerField()
+    cargo_space = models.IntegerField()
+    speed = models.IntegerField()
+
+
+class Planet(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False)
+    size = models.IntegerField(null=False)
+    galaxy = models.IntegerField(null=False)
+    solar_system = models.IntegerField(null=False)
+    position = models.IntegerField(null=False)
+    water = models.IntegerField(default=0)
+    steel = models.IntegerField(default=0)
+    gold = models.IntegerField(default=0)
+    temperature = models.FloatField(null=False)
+    water_farm_lv = models.IntegerField(default=1)
+    gold_mine_lv = models.IntegerField(default=1)
+    steel_mine_lv = models.IntegerField(default=1)
+    fields_used = models.IntegerField(default=0)
+
+    military_power = models.IntegerField(default=0)
+    engine_power = models.IntegerField(default=1)
+    shield_power = models.IntegerField(default=0)
+
+    fleet = models.ManyToManyField(Ship)
+
+
+class Galaxy(models.Model):
+    id = models.IntegerField(primary_key=True)
+
+
+class SolarSystem(models.Model):
+    id = models.IntegerField(primary_key=True)
+    position_1 = models.OneToOneField(Planet, on_delete=models.CASCADE, null=True, related_name='p1')
+    position_2 = models.OneToOneField(Planet, on_delete=models.CASCADE, null=True, related_name='p2')
+    position_3 = models.OneToOneField(Planet, on_delete=models.CASCADE, null=True, related_name='p3')
+    position_4 = models.OneToOneField(Planet, on_delete=models.CASCADE, null=True, related_name='p4')
+    position_5 = models.OneToOneField(Planet, on_delete=models.CASCADE, null=True, related_name='p5')
+    position_6 = models.OneToOneField(Planet, on_delete=models.CASCADE, null=True, related_name='p6')
+    position_7 = models.OneToOneField(Planet, on_delete=models.CASCADE, null=True, related_name='p7')
+    position_8 = models.OneToOneField(Planet, on_delete=models.CASCADE, null=True, related_name='p8')
+    position_9 = models.OneToOneField(Planet, on_delete=models.CASCADE, null=True, related_name='p9')
+    position_10 = models.OneToOneField(Planet, on_delete=models.CASCADE, null=True, related_name='p10')
+    position_11 = models.OneToOneField(Planet, on_delete=models.CASCADE, null=True, related_name='p11')
+    position_12 = models.OneToOneField(Planet, on_delete=models.CASCADE, null=True, related_name='p12')
+    position_13 = models.OneToOneField(Planet, on_delete=models.CASCADE, null=True, related_name='p13')
+    position_14 = models.OneToOneField(Planet, on_delete=models.CASCADE, null=True, related_name='p14')
+    position_15 = models.OneToOneField(Planet, on_delete=models.CASCADE, null=True, related_name='p15')
+    galaxy = models.ForeignKey(Galaxy, on_delete=models.CASCADE, null=True)
+
+
+class Universe(models.Model):
+    galaxies = models.ManyToManyField(Galaxy)
--- api/schema.py
+++ api/schema.py
@@ -1,8 +1,68 @@
 import graphene
 from django.conf import settings
+from api.models import Universe, Galaxy, SolarSystem, Planet
+
+
+class PlanetType(graphene.ObjectType):
+    name = graphene.String()
+    size = graphene.Int()
+    galaxy = graphene.Int()
+    solar_system = graphene.Int()
+    position = graphene.Int()
+    water = graphene.Int()
+    steel = graphene.Int()
+    gold = graphene.Int()
+    temperature = graphene.Float()
+    water_farm_lv = graphene.Int()
+    gold_mine_lv = graphene.Int()
+    steel_mine_lv = graphene.Int()
+    fields_used = graphene.Int()
+    military_power = graphene.Int()
+    engine_power = graphene.Int()
+    shield_power = graphene.Int()
+
+
+class SolarSystemType(graphene.ObjectType):
+    id = graphene.ID()
+    position_1 = graphene.Field(PlanetType)
+    position_2 = graphene.Field(PlanetType)
+    position_3 = graphene.Field(PlanetType)
+    position_4 = graphene.Field(PlanetType)
+    position_5 = graphene.Field(PlanetType)
+    position_6 = graphene.Field(PlanetType)
+    position_7 = graphene.Field(PlanetType)
+    position_8 = graphene.Field(PlanetType)
+    position_9 = graphene.Field(PlanetType)
+    position_10 = graphene.Field(PlanetType)
+    position_11 = graphene.Field(PlanetType)
+    position_12 = graphene.Field(PlanetType)
+    position_13 = graphene.Field(PlanetType)
+    position_14 = graphene.Field(PlanetType)
+    position_15 = graphene.Field(PlanetType)
+
+
+
+class GalaxyType(graphene.ObjectType):
+    id = graphene.ID()
+    solar_systems = graphene.List(SolarSystemType)
+
+    def resolve_solar_systems(self, info, **kwargs):
+        return self.solarsystem_set.all()
+
+
+class UniverseType(graphene.ObjectType):
+    galaxies = graphene.List(GalaxyType)
+
+    def resolve_galaxies(self, info, **kwargs):
+        return self.galaxies.all()
+
 
 
 class Query:
     version = graphene.String()
     def resolve_version(self, info, **kwargs):
         return settings.VERSION
+
+    universe = graphene.Field(UniverseType)
+    def resolve_universe(self, info, **kwargs):
+        return Universe.objects.first()
